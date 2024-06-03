cbc0277b59962409f25b27f2e79989df4499c407
Henry Konno <henry.konno@costcutter.com>
2024-06-03 16:55:12 +0100

    Added an email controller and a viewmodel for the contact form

---
+++ Test.Site/umbraco/controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test.Site.umbraco.models;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Test.Site.umbraco.controllers
+{
+    public class EmailController : SurfaceController
+    {
+        public EmailController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Submit(ContactViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ Test.Site/umbraco/models/ContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.Site.umbraco.models;
+
+public class ContactViewModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone_Number { get; set; }
+    public string MessageBody { get; set; }
+}
